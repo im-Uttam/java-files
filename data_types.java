055747d9be2fd258d393f2a3780f5f757dedad52
Uttam Mukhiya <uttammukhiyaofficial1234@gmail.com>
2026-05-06 15:00:57 +0545

    re commit

---
+++ data_types.java
@@ -0,0 +1,25 @@
+/* this is multiline comment */
+
+/*public class data_types {
+        public static void main(String[] args) {
+                System.out.println("hello");
+            }
+        }*/
+
+// data types: 
+// type varaibleName = value
+public class data_types {
+    public static void main(String[] args) {
+        String myName = "Uttam";
+        int age = 23;
+        double percentage = 65.6;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println("My name is " + myName);
+        System.out.println("I am " + age + " years old");
+        System.out.println("I have scored " + percentage + "%");
+        System.out.println("My grade is " + grade);
+        System.out.println("I am a student: " + isStudent);
+    }
+}
---
+++ identifierss.java
@@ -0,0 +1,23 @@
+/*identifierssn contain letters, digits, underscores, and dollar signs
+Names must begin with a letter
+Names should start with a lowercase letter, and cannot contain whitespace
+Names can also begin with $ and _
+Names are case-sensitive ("myVar" and "myvar" are different variables)
+Reserved words (like Java keywords, such as int or boolean) cannot be used as names 
+*/
+
+/*public class identifiers {
+    public static void main(String[] args) {
+        int myVar = 10;
+        System.out.println(myVar);
+    }
+}
+*/
+
+//constant
+public class identifierss {
+    public static void main(String[] arg) {
+        final float PI = 3.14f, const1 = 20.06f;
+        System.out.println(PI + const1);
+    }
+}
